46362a3d6717dff8a232ab1bfee9aa7ae5c7712c
amruthavr61-code <amruthavr61@gmail.com>
2026-07-07 23:40:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import streamlit as st
 import speech_recognition as sr
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 
 st.set_page_config(page_title="Multilingual Speech Recognition")
 
 st.title("🎤 Multilingual Speech Recognition and Translation System")
 
-st.write("An AI-powered application that converts speech from multiple languages into text and translates it into English.")
+st.write(
+    "An AI-powered application that converts speech from multiple languages into English."
+)
 
 recognizer = sr.Recognizer()
-translator = Translator()
 
 if st.button("Start Recording"):
     with sr.Microphone() as source:
-        st.write("🎙️ Listening... Please speak.")
+        st.write("🎙 Listening... Please speak.")
         recognizer.adjust_for_ambient_noise(source)
 
         try:
@@ -22,19 +23,23 @@ if st.button("Start Recording"):
             text = recognizer.recognize_google(audio)
 
             st.success("Speech Recognized Successfully!")
+
             st.write("Recognized Text:")
             st.write(text)
 
-            translated = translator.translate(text, dest="en")
+            translated = GoogleTranslator(
+                source="auto",
+                target="en"
+            ).translate(text)
 
             st.write("English Translation:")
-            st.success(translated.text)
+            st.success(translated)
 
         except sr.UnknownValueError:
             st.error("Could not understand the speech.")
 
         except sr.RequestError:
-            st.error("Network/API error.")
+            st.error("Speech Recognition service is unavailable.")
 
         except Exception as e:
             st.error(f"Error: {e}")
