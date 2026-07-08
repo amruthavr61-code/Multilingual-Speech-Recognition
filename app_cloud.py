00153c447fc282ce0102d1c834b7d87f7b3f3a06
amruthavr61-code <amruthavr61@gmail.com>
2026-07-08 22:48:50 +0530

    Add files via upload

---
+++ app_cloud.py
@@ -0,0 +1,61 @@
+import streamlit as st
+from streamlit_mic_recorder import mic_recorder
+import assemblyai as aai
+from deep_translator import GoogleTranslator
+aai.settings.api_key = st.secrets["ASSEMBLYAI_API_KEY"]
+st.set_page_config(
+    page_title="Multilingual Speech Recognition",
+    page_icon="🎤",
+    layout="centered"
+)
+
+st.title("🎤 Multilingual Speech Recognition and Translation")
+
+st.write(
+    "Speak in your preferred language and translate it into English."
+)
+
+language_dict = {
+    "English": "en",
+    "Kannada": "kn",
+    "Hindi": "hi",
+    "Tamil": "ta",
+    "Telugu": "te",
+    "Malayalam": "ml"
+}
+
+selected_language = st.selectbox(
+    "Select your language",
+    list(language_dict.keys())
+)
+
+audio = mic_recorder(
+    start_prompt="🎙️ Start Recording",
+    stop_prompt="⏹️ Stop Recording",
+    key="mic"
+)
+
+if audio:
+
+    st.success("🎉 Audio captured successfully!")
+
+    transcriber = aai.Transcriber()
+
+    transcript = transcriber.transcribe(audio["bytes"])
+
+    if transcript.status == "error":
+        st.error(transcript.error)
+
+    else:
+        text = transcript.text
+
+        st.subheader("Recognized Text")
+        st.success(text)
+
+        translated = GoogleTranslator(
+            source="auto",
+            target="en"
+        ).translate(text)
+
+        st.subheader("English Translation")
+        st.success(translated)
